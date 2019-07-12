a188173887e266503f3bdb72703f84e442e5a0a0
Nick Foden <nickfoden@gmail.com>
2019-07-11 23:42:35 -0400

    Cycle flow via the index in order next could be randomization

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import styles from "./styles.module.css";
 
 const Card = ({ item, expand }) => {
   const { answer, link, subject } = item;
-  console.log(expand);
   return (
     <div className={styles.card_container}>
       <h4>{subject}:</h4>
@@ -11,7 +10,7 @@ const Card = ({ item, expand }) => {
         <>
           <p>{answer}</p>
           <a href={link} target="_blank" rel="noopener noreferrer">
-            View Docs
+            View Ref
           </a>
         </>
       )}
--- src/pages/Cycle.js
+++ src/pages/Cycle.js
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 import { Link } from "@reach/router";
 import { getAllCards } from "../api/fauna";
 import Card from "../components/Card";
+import Finished from "../components/Finished";
 import styles from "./pages.module.css";
 class Cycle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCard: 0,
+      show: false
+    };
+  }
+
   componentDidMount() {
     if (!this.props.loaded) {
       getAllCards()
@@ -13,7 +22,18 @@ class Cycle extends Component {
         .then(this.props.change());
     }
   }
+
+  next = () => {
+    this.setState(prevState => {
+      return { currentCard: prevState.currentCard + 1, show: false };
+    });
+  };
+  show = () => {
+    this.setState({ show: true });
+  };
   render() {
+    const { cards } = this.props;
+    const { currentCard, show } = this.state;
     return (
       <div>
         <div>
@@ -21,17 +41,28 @@ class Cycle extends Component {
             <Link to="/">Home</Link>
             <Link to="/new-card">New</Link>
           </header>
-          <section className="flash-body">
-            <ul className="flash-ul">
-              {this.props.cards.map(item => {
-                const { id } = item.ref.value;
-                return (
-                  <li key={id}>
-                    <Card item={item.data} />
-                  </li>
-                );
-              })}
-            </ul>
+          <section className={styles.cycle_body}>
+            {currentCard < cards.length ? (
+              <>
+                {cards[currentCard] && (
+                  <Card item={cards[currentCard]["data"]} expand={show} />
+                )}
+                {!show && (
+                  <button onClick={this.show} className={styles.button}>
+                    {" "}
+                    Show Answer
+                  </button>
+                )}
+                {show && (
+                  <button onClick={this.next} className={styles.button}>
+                    {" "}
+                    Next Question
+                  </button>
+                )}
+              </>
+            ) : (
+              <Finished started={currentCard} />
+            )}
           </section>
         </div>
       </div>
